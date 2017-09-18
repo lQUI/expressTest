a47276a572d02f232a46d0583f6d157fa311fdb3
IQUI <1079029446@qq.com>
2017-09-18 18:27:54 +0800

    modify

--- README.md
+++ README.md
@@ -1,18 +1,57 @@
-# express
-express pratice
-You can clone my repository with Terminal or CMD
-$ git clone https://github.com/lQUI/express.git
+***
 
-$ cd express
+Installation and Use
+==
+在运行项目之前先确保本机已有[nodejs](https://nodejs.org/en)和Mysql<https://github.com/lQUI/express>.
 
-$ npm install
 
-$ node index.js //start node serve
+然后通过cmd输入<br/>
+<code>$ git clone https://github.com/lQUI/express</code><br/>
+<code>$ cd express </code><br/>
+<code>$ npm install</code><br/>
+<code>$ ./insert_example_data.sql.sh   ##导入该练习需要的样本数据</code><br/>
 
-Open the bowser and go to：localhost:3000
 
-You can see "Hello World!" on the web site.
+然后,需要修改[app.js](https://github.com/lQUI/express/blob/master/app.js)里的mysql连接配置信息.
 
-And input localhost:3000/home?title=title.
+然后启动运行
+<code>$ node app.js</code><br/>
 
-You can see 'Hello there!' and the tab will be named 'title'.  
+***
+
+Finished
+==
+
+完成[文档](https://shimo.im/doc/jMyLTKOtyRobZFcB?r=XY7NO9/#magicdomid96)中的任务
+
+定义Routes文件在[app.js](https://github.com/lQUI/express/blob/master/app.js)
+ 
+3.1 Basic Parameterized Http Route<br/>
+操作步骤：浏览器输入[http://localhost:3000/v3/test-api](http://localhost:3000/v3/test-api),如果没显示任何错误信息，则说明已成功访问到。<br/>
+
+
+3.2 Basic Query in Request<br/>
+操作步骤：浏览器输入[http://localhost:3000/v3/plus?a=3&b=4](http://localhost:3000/v3/plus?a=3&b=4)，如果访问成功，浏览器会接受到json字符串：'{ret: 1000, version: 3, action: plus, result: 7}'。<br/>
+
+
+3.3 URLEncoded Form in Request<br/>
+操作步骤：浏览器输入[http://localhost:3000/static/index.html](http://localhost:3000/static/index.html)，然后显示一个表单，填写表单后，点击’submit'按钮提交表单，如果提交成功，浏览器会接受到json字符串：'{ret: 1000, version: 3, action: plus, result: 7}'。<br/>
+
+
+3.4  Html Template Engine Practice<br/>
+操作步骤：浏览器输入[http://localhost:3000/v3/tutorial/student/list](http://localhost:3000/v3/tutorial/student/list)，然后显示一个student的表格。<br/>
+
+
+3.5 Logging to Multiple Files Differentiated by Levels<br/>
+操作步骤：浏览器输入[http://localhost:3000/v3](http://localhost:3000/v3#ltmfdbl)将会打开页面，然后选择需要记录的日志的用户名和日志的级别，点击'add'按钮，将会在该目录下生成cheese.log，而且该文件会记录下此次访问的时间，日志的级别和输出信息'WOW!!!'。
+
+
+3.6 Hiding Your Authentication Protected Service behind AuthMiddleware<br/>
+实现过程:该例子中并没有提供登陆的实现,这里主要根据用户拿到intAuthToken和id通过[app.js](https://github.com/lQUI/express/blob/master/app.js)进行判断,首先根据intAuthToken去token表查询是否有数据,如果有在判断查到的数据里的id是否与request提供的id一致,若一致说明已经登陆了.<br/>
+操作步骤:浏览器输入[http://localhost:3000/sp100029/wallet/self/detail?intAuthToken=xxxyyyzz](http://localhost:3000/sp100029/wallet/self/detail?intAuthToken=xxxyyyzz),则会显示'Welcome "iphone6" user from "192.168.1.88"!',
+如果输入的是[http://localhost:3000/sp100029/wallet/self/detail](http://localhost:3000/sp100029/wallet/self/detail),则显示'{"ret":1001}';<br/>
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var app= express();
+app.use('/static', express.static('public'));
 app.set('view engine', 'jade');
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
+    connectionLimit : 10,
     host : 'localhost',
     user : 'root',
-    password : 'root',
+    password : '123456',
     database : 'testdb'
 });
+
+const log4js = require('log4js');
+log4js.configure({
+    appenders:{
+    cheese:{type: 'file', filename:'cheese.log'},
+    console:{type: 'console'}
+    },
+    categories:{default:{appenders:['cheese','console'], level:'debug'}}
+});
+const userLogger = log4js.getLogger();
+
 
 app.get('/', function(req, res)  {
     res.send('Hello world');
@@ -18,14 +31,14 @@ app.get('/v3/test-api', function(req, res) {
     console.log('hello world');    
 });
 
-app.get('/v:version/:action', function(req, res) {
-    console.log(req.params.version + req.params.action + req.query.a + req.query.b );
-    var result = parseInt(req.query.a) + parseInt(req.query.b);
+app.post('/v:version/:action', function(req, res) {
+    console.log(req.params.version + req.params.action + req.query.a + req.params );
+    var result = parseInt(req.a) + parseInt(req.b);
     res.jsonp({
        ' ret' : 1000,
         'version' : req.params.version,
         'action' :req.params.action,
-        'result' : result
+        'result' :req.body  
     }); 
 });
 
@@ -41,16 +54,54 @@ app.get('/v:version/:namespace/:resource/:action', function (req, res) {
     res.render('index', {students : students});
 });
 
-app.use('/:userId/wallet/self/detail', function(req,res) {
-    if('' != req.query.intAuthToken){
-        connection.connect();
-        connection.query('select * from token where intAuthToken = ?',[req.query.intAuthToken], function(error, results, fields){
-            if (error)  throw error;
-            console.log(results);
-            console.log(fields);
+app.post('/v:version/:resource/:level/:action', function(req,res) {
+    console.log('enter');
+    userLogger.log(req.params.level,'WOW!!!');  
+    res.send({ret : 1000});
+});
+
+app.get('/:userId/wallet/self/detail', function(req, res, next) {
+    var isPass = false;
+    if('' != req.query.intAuthToken){//判断intAuthToken是否存在
+        var token ;
+        pool.getConnection(function(err, connection){
+		    connection.query('select * from token where intAuthToken = ?',[req.query.intAuthToken], function(error, results, fields){
+		        if (error)  throw error;
+		        token = results[0];
+		        if(token == undefined){//根据intAuthToken查询是否为空
+			        console.log('user is not found');
+			        res.send({ret : 1001});
+		        }else{
+			        pool.getConnection(function(err, connection){
+			            connection.query('select * from user where id = ?',[token.id],function(error, results, fields){
+				            if (error)  throw error;
+
+				            if(results.length > 0 && results[0].id == req.params.userId){//判断用户是否合法
+                                isPass=true; console.log('pass!! '+isPass);
+                            }
+
+				            if(isPass){
+                                res.render('user', {token : token, user :results[0]});
+				            }else{
+		                        console.log('user.id is not match userId');
+				                res.send({ret : 1001});
+				            }
+			            });
+			            connection.release();
+		                //console.log(results);
+			        });
+		        }
+		    });
+            connection.release();
         });
+    }else{
+        console.log('intAuthToken is null');
+        res.send({ret : 1001});
     }
-    res.send({ret : 1001});
+});
+
+app.get('/:userId/wallet/self/detail', function(req, res) {
+    
 });
 
 var server = app.listen (3000, function () {
